b51df98a83e372d67c4d3887ae1d1c3daba4d95e
FriedFeid <friedelcat@gmx.de>
2023-10-10 12:15:34 +0200

    Improved Dataset with choosable start stop

--- dataset_test.py
+++ dataset_test.py
@@ -1,17 +1,34 @@
 from ldm.data.synthetic_data import BlockWorld
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 
-dset = BlockWorld(size=None, control=['Depth', 'Env'])
+dset = BlockWorld(
+                 size=None, random_resized_crop=False,
+                 interpolation='bicubic', scale=[1.0, 1.0], control=['Depth', 'Env', 'SSphere'],
+                 full_set=False, data_start=50_000, data_stop=50_400,
+                 data_root=None,
+                 use_pillow=True
+                  )
 ex = dset[0]
 # dset = LaionValidation(size=256)
 # ex = dset[0]
-for k in ["image", "caption", 'control_grid'
-            ]:
+
+for k in ["image", "caption", 'control_grid']:
     print(type(ex[k]))
     try:
         print(ex[k].shape)
     except:
         print(ex[k])
-    if k =='control_grid':
+    if k == 'control_grid':
         plt.imshow(ex[k])
         plt.savefig('control_gird.png')
+
+train_dataloader = DataLoader(dset, batch_size=8, shuffle=True)
+
+train_batch = next(iter(train_dataloader))
+for k in ["image", "caption", 'control_grid']:
+    print(type(train_batch[k]))
+    try:
+        print(train_batch[k].shape)
+    except:
+        print(train_batch[k])
--- ldm/data/synthetic_data.py
+++ ldm/data/synthetic_data.py
@@ -1,17 +1,16 @@
 import os
 import numpy as np
 import cv2
-import csv 
-# import albumentations
+import csv
+import albumentations
 from PIL import Image
 from torch.utils.data import Dataset
-import json
 
 import torchvision.transforms as tt
 import torch
 import einops
 import OpenEXR
-import Imath 
+import Imath
 
 # from annotator.canny import CannyDetector
 # from annotator.util import HWC3
@@ -19,37 +18,21 @@ import Imath
 # from light_util import get_perspective
 
 from ldm.data.util import create_image_grid
-from skimage import feature
-from glob import glob 
+
+from glob import glob
 from natsort import natsorted
-import yaml
 
 
 class BlockWorld(Dataset):
-    def __init__(self, size=None, random_resized_crop=False, 
-                 interpolation='bicubic', scale=[1.0, 1.0], control=['Depth'], 
-                 full_set=True, data_csv=False, data_root=None, 
-                 use_pillow=True, use_edges=True, use_render=True, use_distance=True, 
-                 use_depth_maps=False):
-        
-        self.use_edges = use_edges
-        self.use_render = use_render
-        self.use_distance = use_distance
-        self.use_depth_maps = use_depth_maps
-
-        if data_csv is None:
-            # TODO: implement splitting dataset
-            self.split = self.get_split()
-            self.data_csv = {"train": "data/sun360_light_train_split.txt",
-                            "validation": "data/sun360_light_train_split.txt"}[self.split]
-        else: 
-            self.data_csv = data_csv
-            if not full_set:
-                self.split = self.get_split()
+    def __init__(self, size=None, random_resized_crop=False,
+                 interpolation='bicubic', scale=[1.0, 1.0], control=['Depth'],
+                 full_set=True, data_start=0, data_stop=50_000,
+                 data_root=None,
+                 use_pillow=True):
 
         if data_root is None:
             self.data_root = '/export/data/vislearn/rother_subgroup/feiden/data/ControlNet/training/Generated_Data'
-        else: 
+        else:
             self.data_root = data_root
 
         self.use_pillow = use_pillow
@@ -57,11 +40,21 @@ class BlockWorld(Dataset):
         self.full_set = full_set
 
         self.Labels = self.__load_labels__()
-        
+
         self.Render_folder = os.path.join(self.data_root, 'Render')
-        self.Renders = natsorted(glob(os.path.join(self.Render_folder, 'Render_*.png') ))
+        self.Renders = natsorted(glob(os.path.join(self.Render_folder, 'Render_*.png')))
         self.__len = len(self.Renders)
-        
+
+        if self.full_set:
+            self.data_start = 0
+            self.data_stop = self.__len
+        else:
+            self.data_start = data_start
+            self.data_stop = data_stop
+
+            self.Renders = self.Renders[data_start:data_stop]
+            self.__len = len(self.Renders)
+
         self.hint_dic = {}
         for key in ['Depth', 'DisMap', 'Edges', 'SSphere', 'Env']:
             # Collecting all Folder Paths
@@ -71,19 +64,19 @@ class BlockWorld(Dataset):
                 self.hint_dic[key+'_folder'] = os.path.join(self.data_root, 'Distance_Map')
             elif key == 'SSphere':
                 self.hint_dic[key+'_folder'] = os.path.join(self.data_root, 'SpikySphere')
-            else: 
+            else:
                 self.hint_dic[key+'_folder'] = os.path.join(self.data_root, key)
             # Loading paths to Images
             if key == 'Env':
-                self.hint_dic[key] = natsorted(glob(os.path.join(self.hint_dic[key+'_folder'], key + '_*.exr'))) 
-            else: 
-                self.hint_dic[key] = natsorted(glob(os.path.join(self.hint_dic[key+'_folder'], key + '_*.png')))
-        
+                self.hint_dic[key] = natsorted(glob(os.path.join(self.hint_dic[key+'_folder'], key + '_*.exr')))[data_start:data_stop]
+            else:
+                self.hint_dic[key] = natsorted(glob(os.path.join(self.hint_dic[key+'_folder'], key + '_*.png')))[data_start:data_stop]
+
         self.consistant = self.__consistant__()
 
         self.control = control
 
-        size = None if size is not None and size<=0 else size
+        size = None if size is not None and size <= 0 else size
         self.size = size
 
         if use_pillow:
@@ -100,7 +93,7 @@ class BlockWorld(Dataset):
                     antialias=True
                     )
             if self.size is not None:
-                
+
                 self.image_rescaler = tt.Resize(
                     size=self.size,
                     interpolation=self.interpolation,
@@ -135,20 +128,17 @@ class BlockWorld(Dataset):
                     "area": cv2.INTER_AREA,
                     "lanczos": cv2.INTER_LANCZOS4}[self.interpolation]
             self.env_rescaler = albumentations.SmallestMaxSize(max_size=(256, 512),
-                                                                    interpolation=self.interpolation)
-            
+                                                               interpolation=self.interpolation)
 
             if self.size is not None:
-                
-                
                 new_env_size = self.size
                 new_env_size = (int(new_env_size/2), int(new_env_size))
 
                 self.env_rescaler = albumentations.SmallestMaxSize(max_size=new_env_size,
-                                                                    interpolation=self.interpolation)
+                                                                   interpolation=self.interpolation)
 
                 self.image_rescaler = albumentations.SmallestMaxSize(max_size=self.size,
-                                                                    interpolation=self.interpolation)
+                                                                     interpolation=self.interpolation)
                 # self.segmentation_rescaler = albumentations.SmallestMaxSize(max_size=self.size,
                 #                                                             interpolation=cv2.INTER_NEAREST)
                 # self.center_crop = not (random_crop or random_resized_crop)
@@ -162,12 +152,10 @@ class BlockWorld(Dataset):
                     self.cropper = albumentations.RandomCrop(height=self.size, width=self.size)
                 self.preprocessor = self.cropper
 
-
-
     def __load_exr__(self, exr_path):
         '''
-        loads exr data: 
-        
+        loads exr data:
+
         returns:
             sun: np.array (hight, width, channels)
         '''
@@ -185,38 +173,39 @@ class BlockWorld(Dataset):
         # Fill numpy array
         arr = np.zeros((size[0], size[1], 3), dt)
         for i, c in enumerate(['R', 'G', 'B']):
-            arr[:,:,i] = np.frombuffer(exr.channel(c), dt).reshape(size)
-        
+            arr[:, :, i] = np.frombuffer(exr.channel(c), dt).reshape(size)
+
         exr = arr.astype(np.float32)
-                
+
         return exr
 
     def __consistant__(self):
         '''
         Checks if the Dataset is self consistant
 
-        returns:   
-            consitant: bool 
+        returns:
+            consitant: bool
         '''
         amount_data = len(self.Renders)
-        consis = True 
-        if amount_data != len(self.Labels):
-            consis = False
+        consis = True
+        if self.full_set:
+            if amount_data != len(self.Labels):
+                consis = False
 
         for key in ['Depth', 'DisMap', 'Edges', 'SSphere', 'Env']:
             if amount_data != len(self.hint_dic[key]):
                 consis = False
-        
+
         if not consis:
             print('WARNING: There are inconsitencys in the datase: ')
             print(f'Renders: {len(self.Renders)}')
             print(f'Labels: {len(self.Labels)}')
             for key in ['Depth', 'DisMap', 'Edges', 'SSphere', 'Env']:
-                print(f'{key}: {len(self.hint_dic[key])}') 
+                print(f'{key}: {len(self.hint_dic[key])}')
             return False
-        else: 
+        else:
             return True
-    
+
     def __load_labels__(self):
         '''
         Loads Labels cvs as list of dictionarys.
@@ -224,13 +213,13 @@ class BlockWorld(Dataset):
         param_dic = {}
         with open(os.path.join(self.data_root, 'Labels.csv'), newline='') as tab:
             reader = csv.DictReader(tab, delimiter=',', )
-            for row in reader: 
+            for row in reader:
                 dic = {}
-                for key in row: 
+                for key in row:
                     if key == 'wsun' or key == 'wsky':
                         vec = np.array([float(row[key].split(' ')[0]),
-                                            float(row[key].split(' ')[1]),
-                                            float(row[key].split(' ')[2]),])
+                                        float(row[key].split(' ')[1]),
+                                        float(row[key].split(' ')[2]),])
                         dic[key] = vec
                     elif key == 'name' or key == 'Prompt':
                         dic[key] = row[key]
@@ -239,76 +228,75 @@ class BlockWorld(Dataset):
                 param_dic[row['name']] = dic
 
         return param_dic
-        
+
     def __len__(self):
         if self.consistant:
             return self.__len
         else:
             return len(self.Labels)
-    
+
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        
-        image_render = Image.open( self.Renders[idx] )
+
+        image_render = Image.open(self.Renders[idx])
         if not image_render.mode == 'RGB':
             image_render = image_render.convert('RGB')
         image_render = np.array(image_render).astype(np.uint8)
 
-
         instance_ = {}
         instance_['render'] = tt.ToTensor()(image_render)
-        for key in self.control: 
+        for key in self.control:
             if key == 'Env':
                 img = torch.from_numpy(np.moveaxis(self.__load_exr__(self.hint_dic[key][idx]), -1, 0))
-                
+
                 img = self.env_rescaler(img)
                 if self.size is not None:
                     full_img = torch.zeros((3, self.size, self.size))
                     img_shape = img.size()
                     full_img[:, :img_shape[1], :img_shape[2]] = img
-                
-                else: 
+
+                else:
                     full_img = torch.zeros_like(instance_['render'])
                     full_img[:, 128:384, :] = img
                 instance_[key] = full_img
-            
+
             elif key == 'DisMap':
                 img = Image.open(self.hint_dic[key][idx])
                 if not img.mode == 'RGB':
                     img = img.convert('RGB')
                 img = np.array(img).astype(np.uint8)
-                img = tt.ToTensor()(img) 
+                img = tt.ToTensor()(img)
 
                 img = self.env_rescaler(img)
                 if self.size is not None:
                     full_img = torch.zeros((3, self.size, self.size))
                     img_shape = img.size()
                     full_img[:, :img_shape[1], :img_shape[2]] = img
-                
-                else: 
+
+                else:
                     full_img = torch.zeros_like(instance_['render'])
                     full_img[:, 128:384, :] = img
                 instance_[key] = full_img
 
-            else: 
+            else:
                 img = Image.open(self.hint_dic[key][idx])
                 if not img.mode == 'RGB':
                     img = img.convert('RGB')
                 img = np.array(img).astype(np.uint8)
 
                 img = tt.ToTensor()(img)
-            
+
                 full_img = img
                 instance_[key] = full_img
 
-        if self.size is not None: 
-            for key in instance_: 
+        if self.size is not None:
+            for key in instance_:
                 instance_[key] = self.image_rescaler(instance_[key])
 
         for key in instance_:
             instance_[key] = np.array(einops.rearrange(instance_[key], 'c h w -> h w c')).astype(np.float32)
-        
+
         hints = []
         controls = 0
         grid = [instance_['render']]
@@ -316,40 +304,41 @@ class BlockWorld(Dataset):
             if key == 'Env':
                 hints.append(instance_[key])
                 grid.append(instance_[key].clip(0, 1))
-                controls +=1
+                controls += 1
             else:
                 hints.append(instance_[key])
                 grid.append(instance_[key])
-                controls +=1
-        
+                controls += 1
+
         example = {}
         example['hint'] = np.concatenate(hints, axis=2)
         example['image'] = instance_['render']/127.5 - 1.0
-        
+
         keys = self.hint_dic['Env'][idx].split('.exr')[0]
         if '/' in keys:
             keys = keys.split('/')[-1]
         example['caption'] = self.Labels[keys]['Prompt']
+        print(keys, self.hint_dic[key][idx].split('/')[-1])
 
         example['control_grid'] = create_image_grid(np.stack(grid), (1, controls + 1))
 
         return example
-                
+
 
 class BlockWorldTrain(BlockWorld):
     # default to random_crop=True
-    def __init__(self, size=None, random_resized_crop=False, 
-                 interpolation='bicubic', scale=[1.0, 1.0], control=['Depth'], 
-                 full_set=True, data_csv=False, data_root=None, 
-                 use_pillow=True, use_edges=True, use_render=True, use_distance=True, 
-                 use_depth_maps=False
-                ):
+    def __init__(self, size=None, random_resized_crop=False,
+                 interpolation='bicubic', scale=[1.0, 1.0], control=['Depth'],
+                 full_set=True, data_start=0, data_stop=50_000,
+                 use_pillow=True, data_root=None
+                 ):
         super().__init__(
             size=size, interpolation=interpolation,
             scale=scale, random_resized_crop=random_resized_crop,
-            control=control, data_root=data_root, data_csv=data_csv,
-            use_edges=use_edges, use_render=use_render, use_distance=use_distance,
-            use_depth_maps=use_depth_maps,
+            control=control,
+            full_set=full_set, data_start=data_start,
+            data_stop=data_stop,
+            use_pillow=use_pillow, data_root=data_root,
                           )
 
     def get_split(self):
@@ -357,17 +346,19 @@ class BlockWorldTrain(BlockWorld):
 
 
 class BlockWorldValidation(BlockWorld):
-    def __init__(self, size=None, random_resized_crop=False, 
-                 interpolation='bicubic', scale=[1.0, 1.0], control=['Depth'], 
-                 full_set=True, data_csv=False, data_root=None, 
-                 use_pillow=True, use_edges=True, use_render=True, use_distance=True, 
-                 use_depth_maps=False
-                  ):
-        super().__init__(size=size, interpolation=interpolation,
-                         control=control, data_root=data_root,
-                         data_csv=data_csv, use_edges=use_edges, use_render=use_render,
-                         use_distance=use_distance, use_depth_maps=use_depth_maps)
+    def __init__(self, size=None, random_resized_crop=False,
+                 interpolation='bicubic', scale=[1.0, 1.0], control=['Depth'],
+                 full_set=True, data_start=0, data_stop=50_000,
+                 use_pillow=True, data_root=None
+                 ):
+        super().__init__(
+            size=size, interpolation=interpolation,
+            scale=scale, random_resized_crop=random_resized_crop,
+            control=control,
+            full_set=full_set, data_start=data_start,
+            data_stop=data_stop,
+            use_pillow=use_pillow, data_root=data_root,
+                         )
 
     def get_split(self):
         return "validation"
-
